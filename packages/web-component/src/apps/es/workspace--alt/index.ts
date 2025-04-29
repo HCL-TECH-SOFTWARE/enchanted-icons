d7ea24fe307ad203bbf3a4e8ea8c90353e9d8ded
Nhicko Jerome Garachico <nhickojerome.garac@hcl-software.com>
2025-04-29 21:51:05 +0800

    DXQ-43136: update imports on workspace--alt icon

--- packages/web-component/src/apps/es/workspace--alt/index.ts
+++ packages/web-component/src/apps/es/workspace--alt/index.ts
@@ -13,8 +13,8 @@
  * limitations under the License.                                           *
  * ======================================================================== */
 
-import { css, html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { html } from 'lit';
+import { customElement } from 'lit/decorators.js';
 import { createSvgIcon, IIconAttrs, IIconContent } from '../../../utils';
 import { BaseIcon } from '../../../utils/base-icon';
 
