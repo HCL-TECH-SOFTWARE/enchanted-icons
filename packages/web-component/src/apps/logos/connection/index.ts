309a0018929517b2c8b3597c9ecda7d34331d2cb
Hanjo Hagemeier <hanjo.hagemeier@hcl.com>
2026-03-05 15:52:08 +0100

    DXQ-48163 - add a enchanted component scoping logic

--- packages/web-component/src/apps/logos/connection/index.ts
+++ packages/web-component/src/apps/logos/connection/index.ts
@@ -16,8 +16,9 @@
 import { css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { BaseIcon } from '../../../utils/base-icon';
+import { ICON_PREFIX } from '../../../utils/tags';
 
-export const ICON_NAME = 'svg-connections';
+export const ICON_NAME = `${ICON_PREFIX}svg-connections`;
 @customElement(ICON_NAME)
 export class SvgConnections extends BaseIcon {
   static styles = css`
--- packages/web-component/src/apps/logos/portal/index.ts
+++ packages/web-component/src/apps/logos/portal/index.ts
@@ -16,8 +16,9 @@
 import { css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { BaseIcon } from '../../../utils/base-icon';
+import { ICON_PREFIX } from '../../../utils/tags';
 
-export const ICON_NAME = 'svg-portal';
+export const ICON_NAME = `${ICON_PREFIX}svg-portal`;
 @customElement(ICON_NAME)
 export class SvgPortal extends BaseIcon {
   static styles = css`
